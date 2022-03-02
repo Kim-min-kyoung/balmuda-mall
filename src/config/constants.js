534802982203e9bcd526a2651293bf398a52e79c
Kim-min-kyoung <alsrudghtkd@naver.com>
2022-03-03 00:07:09 +0900

    api_url 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = process.env.NODE_ENV === "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === 'production'
+? "https://balmuda-mall-server.herokuapp.com/"
+: "http://localhost:8080";
